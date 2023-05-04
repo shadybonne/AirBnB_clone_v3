3d231c25e21536eb6f1f092d9835c8ebb8713c51
janeamy <jane.ammie@gmail.com>
2023-05-04 12:45:23 -0700

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains endpoint(route) status
+Module contains endpoint(route) status.
 """
 from models import storage
 from flask import Flask
@@ -11,6 +11,19 @@ from flask import jsonify
 @app_views.route('/status', strict_slashes=False)
 def status():
     """
-    Returns a JSON status
+    Returns a JSON status.
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
